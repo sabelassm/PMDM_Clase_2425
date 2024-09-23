ac92a40de9cab250c001373f3c9207970fa6bcca
sabela <sabela@a26eql00.sanclemente.local>
2024-09-23 12:56:13 +0200

    chronos

--- UD01_3_Chronos/app/src/main/java/com/example/ud01_3_chronos/MainActivity.kt
+++ UD01_3_Chronos/app/src/main/java/com/example/ud01_3_chronos/MainActivity.kt
@@ -1,12 +1,29 @@
 package com.example.ud01_3_chronos
 
 import android.os.Bundle
+import android.os.SystemClock
+import android.widget.Button
+import android.widget.Chronometer
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 
 class MainActivity : AppCompatActivity() {
+    val RUNNING_KEY = "running"
+    val OFFSET_KEY = "offset"
+    val BASE_KEY = "base"
+
+    lateinit var chrono: Chronometer
+    var running = false
+    var offset = 0L
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        outState.putBoolean(RUNNING_KEY, running)
+        outState.putLong(OFFSET_KEY, offset)
+        outState.putLong(BASE_KEY, chrono.base)
+        super.onSaveInstanceState(outState)
+    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +33,38 @@ class MainActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+        chrono = findViewById<Chronometer>(R.id.chrTemporizador)
+
+        if(savedInstanceState!=null){
+            offset = savedInstanceState.getLong(OFFSET_KEY)
+            running = savedInstanceState.getBoolean(RUNNING_KEY)
+            if(running){
+                chrono.base = savedInstanceState.getLong(BASE_KEY)
+                chrono.start()
+            }
+        }
+        //Funcionalidad Botones
+        val btnStart = findViewById<Button>(R.id.btnStart)
+        btnStart.setOnClickListener{
+            if(!running){
+                chrono.base = SystemClock.elapsedRealtime()- offset
+                chrono.start()
+                running = true
+            }
+        }
+        val btnPause = findViewById<Button>(R.id.btnPause)
+        btnPause.setOnClickListener{
+            if(running){
+                offset = SystemClock.elapsedRealtime() - chrono.base
+                chrono.stop()
+                running = false
+            }
+        }
+        val btnRestart = findViewById<Button>(R.id.btnRestart)
+        btnRestart.setOnClickListener{
+            offset=0L
+            chrono.base = SystemClock.elapsedRealtime()
+        }
+
     }
 }
