1baea2ece8f6c95abc03d1d1a6a47746ed5066bc
sabela <sabela@a26eql00.sanclemente.local>
2024-10-07 10:57:54 +0200

    Fragmentos

--- UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/EncryptFragment.kt
+++ UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/EncryptFragment.kt
@@ -6,13 +6,15 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 
-class WelcomeFragment : Fragment() {
+class EncryptFragment : Fragment() {
+
+
     override fun onCreateView(
-        inflater: LayoutInflater,
-        container: ViewGroup?,
+        inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_welcome, container, false)
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_encrypt, container, false)
     }
 
 }
