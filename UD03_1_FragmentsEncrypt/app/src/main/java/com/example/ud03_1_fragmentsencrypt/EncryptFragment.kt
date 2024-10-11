7399aba97283246333ec9f5de8fe7a0039f6970c
sabela <sabela@a26eql00.sanclemente.local>
2024-10-11 14:26:21 +0200

    layout

--- UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/EncryptFragment.kt
+++ UD03_1_FragmentsEncrypt/app/src/main/java/com/example/ud03_1_fragmentsencrypt/EncryptFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
 
 class EncryptFragment : Fragment() {
 
@@ -16,7 +17,19 @@ class EncryptFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_encrypt, container, false)
         val mensaje = EncryptFragmentArgs.fromBundle(requireArguments()).message
+        val textoEncriptado = view.findViewById<TextView>(R.id.txtViewMessageEncrypted)
+        textoEncriptado.text = cifra(mensaje)
         return view
     }
+
+    fun cifra(msg : String) =
+        msg.map {
+            if(it.isLetter())
+                it.uppercaseChar().code
+                    .minus('A'.code).plus(3)
+                    .mod(26).plus('A'.code).toChar()
+            else
+                it
+        }.joinToString("")
 
 }
